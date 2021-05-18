2d4a4c42b48524c010906bbded933c3b10d9ad10
Aaron Yang <zhenhuayang@outlook.com>
2021-05-17 21:57:05 -0500

    update

--- components/src/ApprovalCard.js
+++ components/src/ApprovalCard.js
@@ -1,17 +1,9 @@
 import React from 'react';
-import faker from 'faker';
-import CommentDetail  from './CommentDetail';
 
-const ApprovalCard = () => {
+const ApprovalCard = (props) => {
     return (
         <div className="ui card">
-            <CommentDetail 
-                author="Sam" 
-                timeAgo="Today at 4:12PM" 
-                content="Hi, it works."
-                avatar={faker.image.avatar()}
-            />
-            <div className="content">Are you sure?</div>
+            <div className="content">{props.children}</div>
             <div className="extra content">
                 <div className="ui two buttons">
                     <div className="ui basic green button">Approve</div>
--- components/src/index.js
+++ components/src/index.js
@@ -7,13 +7,20 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
     return (
         <div className="ui container comments">
-            <ApprovalCard/>
-            <CommentDetail 
-                author="Sam" 
-                timeAgo="Today at 4:12PM" 
-                content="Hi, it works."
-                avatar={faker.image.avatar()}
-            />
+            <ApprovalCard>
+                <div>
+                    <h4>Warning!</h4>
+                    <p>Do you really do this?</p>
+                </div>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Sam" 
+                    timeAgo="Today at 4:12PM" 
+                    content="Hi, it works."
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
             <CommentDetail 
                 author="Alex" 
                 timeAgo="Today at 1:12PM" 
